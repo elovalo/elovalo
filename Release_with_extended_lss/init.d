054743eccb88fb287347c552f2d12dc74d627101
Mio Taalas <Mtaalas@gmail.com>
2012-07-17 22:17:11 +0300

    Updated gitignore and removed unnecessary binary files

--- Release_with_extended_lss/init.d
+++
@@ -1,5 +0,0 @@
-init.d init.o: ../init.c ../pinMacros.h ../init.h
-
-../pinMacros.h:
-
-../init.h:
--- Release_with_extended_lss/main.d
+++
@@ -1,9 +0,0 @@
-main.d main.o: ../main.c ../pinMacros.h ../main.h ../init.h ../tlc5940.h
-
-../pinMacros.h:
-
-../main.h:
-
-../init.h:
-
-../tlc5940.h:
