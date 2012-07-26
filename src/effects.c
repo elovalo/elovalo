9c363be60d89d39e73a4f001bddee40e83fd8a78
Juho Vepsalainen <bebraw@gmail.com>
2012-07-26 17:37:04 +0300

    Add worm effect

--- src/effects.c
+++ src/effects.c
@@ -12,12 +12,14 @@
 #include "effect_utils.h"
 
 static void init_brownian(void);
+static void init_worm(void);
 
 const effect_t effects[] = {
 	{ "brownian", &init_brownian, &effect_brownian, 1000, 0 },
 	{ "sine", NULL, &effect_sine, 1000, 1 },
 	{ "wave", NULL, &effect_wave, 1000, 1 },
 	{ "sphere", NULL, &effect_sphere, 1000, 1 },
+	{ "worm", &init_worm, &effect_worm, 6000, 0 },
 	{ "const", NULL, &effect_constant, 1000, 1 },
 	{ "layers", NULL, &effect_layers_tester, 1000, 1 }
 };
@@ -43,6 +45,8 @@ void effect_brownian(void)
 	brown_x = clamp(brown_x + randint(-2, 2), 0, LEDS_X - 1);
 	brown_y = clamp(brown_y + randint(-2, 2), 0, LEDS_Y - 1);
 	brown_z = clamp(brown_z + randint(-2, 2), 0, LEDS_Z - 1);
+
+	clear_buffer();
 }
 
 /**
@@ -91,6 +95,37 @@ void effect_sphere(void)
 			}
 		}
 	}
+}
+
+/**
+ * Plot worm.
+ */
+uint16_t worm_pos[3];
+uint16_t worm_dir;
+int worm_speed;
+static void init_worm(void)
+{
+	worm_pos[0] = 4;
+	worm_pos[1] = 4;
+	worm_pos[2] = 4;
+	worm_dir = 0;
+	worm_speed = 1;
+
+	clear_buffer();
+}
+void effect_worm(void)
+{
+	set_led(worm_pos[0], worm_pos[1], worm_pos[2], (1<<GS_DEPTH) - 1);
+
+	// Relies on the fact that it's a cube
+	// Not entirely fool proof but probably good enough
+	int new_pos = worm_pos[worm_dir] + worm_speed;
+	if(new_pos < 0 || new_pos >= LEDS_X || randint(0, 10) > 7) {
+		worm_dir = worm_dir + 1 > 2? 0: worm_dir + 1;
+		worm_speed = -worm_speed;
+	}
+
+	worm_pos[worm_dir] += worm_speed;
 }
 
 /**
