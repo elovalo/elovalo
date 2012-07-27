058d66bde0494d568054e8a037a2ab8de99ff329
Juho Vepsalainen <bebraw@gmail.com>
2012-07-27 13:57:05 +0300

    Move brownian clear to the right place
    Previously it would clear on each update making the effect blank
    Better to have that on init.

--- src/effects.c
+++ src/effects.c
@@ -37,6 +37,8 @@ static void init_brownian(void)
 	brown_x = (uint8_t)(LEDS_X / 2);
 	brown_y = (uint8_t)(LEDS_Y / 2);
 	brown_z = (uint8_t)(LEDS_Z / 2);
+
+	clear_buffer();
 }
 void effect_brownian(void)
 {
@@ -45,8 +47,6 @@ void effect_brownian(void)
 	brown_x = clamp(brown_x + randint(-2, 2), 0, LEDS_X - 1);
 	brown_y = clamp(brown_y + randint(-2, 2), 0, LEDS_Y - 1);
 	brown_z = clamp(brown_z + randint(-2, 2), 0, LEDS_Z - 1);
-
-	clear_buffer();
 }
 
 /**
