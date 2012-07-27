de32aef8012b828dfcedfca27e436bf4799d57e9
Juho Vepsalainen <bebraw@gmail.com>
2012-07-27 14:11:57 +0300

    Make it possible to output in hd
    Added --hd flag to anim and frame for this. Revamped those to use
    argparse while at it. Now you can get some basic help using either
    -h or --help flags (standard).

--- simulator/init.py
+++ simulator/init.py
@@ -1,12 +1,28 @@
+import argparse
 import os
-import sys
 from subprocess import call
 
-os.environ['effect'] = sys.argv[1]
-os.environ['path'] = sys.argv[2]
 
-os.chdir('..')
-call('scons --no-avr', shell=True)
-os.chdir('simulator')
-call('../build_exporter/exporter', shell=True)
+def parser():
+    p = argparse.ArgumentParser()
+    p.add_argument('effect')
+    p.add_argument('path')
+    p.add_argument("--hd", help="Render in HD", action="store_true")
 
+    return p
+
+
+def set_env(args):
+    os.environ['effect'] = args.effect
+    os.environ['path'] = args.path
+
+    if args.hd:
+        os.environ['hd'] = '1'
+
+
+def execute(args):
+    set_env(args)
+    os.chdir('..')
+    call('scons --no-avr', shell=True)
+    os.chdir('simulator')
+    call('../build_exporter/exporter', shell=True)
