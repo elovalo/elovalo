92587d17642b08bccba4d54258322900389857a0
Juho Vepsäläinen <bebraw@gmail.com>
2012-08-27 16:59:30 +0300

    Remove redundant comment

--- src/effects.h
+++ src/effects.h
@@ -1,7 +1,4 @@
 #include "effects/lib/utils.h"
 
-/* Individual effect declarations are private and are moved to
-   effects.c */
-
 extern const effect_t effects[];
 extern const uint8_t effects_len;
