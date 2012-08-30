6760cfc0672eb3b88775aa479a350de8ec403ccf
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-08-30 23:35:52 +0300

    Put clock to .noinit and validate clock variables using checksum
    Checksum algorithm is XOR using 0x55 initial value and is calculated
    from all bytes of posix_time and posix_ticks. Checksum is validated
    every 8ms. Clock survives resets (but not power down, of course).
    
    Related to #10 and closes #13.

--- src/avr/timer.c
+++ src/avr/timer.c
@@ -10,7 +10,16 @@ static volatile uint16_t ticks_volatile = 0;
 /* Real time clock is 32 bit second counter starting from 1970-01-01
  * 00:00:00 +0000 (UTC). This counter is unsigned, so expect it to
  * work until 2016. */
-static volatile uint32_t posix_time = 0;
+static volatile uint32_t posix_time __attribute__ ((section (".noinit")));
+static volatile uint8_t posix_time_div __attribute__ ((section (".noinit")));
+static volatile uint8_t posix_time_cksum __attribute__ ((section (".noinit")));
+
+/* Private functions */
+static uint8_t is_time_valid(void);
+static uint8_t calc_posix_time_cksum(void);
+
+/* Dividing 8 ms interval with 125 to get exactly 1 second */
+#define POSIX_DIVIDER 125
 
 /**
  * Increments millisecond counter.
@@ -20,12 +29,14 @@ ISR(TIMER2_COMPA_vect)
 	// Tick counter for effects
 	ticks_volatile++;
 
-	/* Count seconds by dividing 8 ms interval with 125 */
-	static uint8_t div = 125;
-	if (posix_time && !--div) {
-		// Exactly one second has passed
-		div = 125;
-		posix_time++;
+	// Run real time clock if it is set
+	if (is_time_valid()) {
+		if (!--posix_time_div) {
+			posix_time_div = POSIX_DIVIDER;
+			posix_time++;
+		}
+		// Update checksum
+		posix_time_cksum = calc_posix_time_cksum();		
 	}
 }
 
@@ -52,6 +63,8 @@ void reset_time(void) {
 int stime(time_t *t) {
 	ATOMIC_BLOCK(ATOMIC_FORCEON) {
 		posix_time = *t;
+		posix_time_div = POSIX_DIVIDER;
+		posix_time_cksum = calc_posix_time_cksum();
 	}
 	return 0;
 }
@@ -63,8 +76,29 @@ int stime(time_t *t) {
 time_t time(time_t *t) {
 	uint32_t time;
 	ATOMIC_BLOCK(ATOMIC_FORCEON) {
-		time = posix_time;
+		// Time is zero when it's not set
+		time = is_time_valid() ? posix_time : 0;
  	}
 	if (t != NULL) *t = time;
 	return time;
+}
+
+/**
+ * Calculates checksum using XOR and
+ * seed value of 0x55.
+ */
+static uint8_t calc_posix_time_cksum(void) {
+	return ((uint8_t)(posix_time >> 24) ^
+		(uint8_t)(posix_time >> 16) ^
+		(uint8_t)(posix_time >> 8) ^
+		(uint8_t)posix_time_div ^
+		(uint8_t)0x55);
+}
+
+/**
+ * Validates checksum. May be called only when interrupts are
+ * disabled.
+ */
+static uint8_t is_time_valid(void) {
+	return posix_time_cksum == calc_posix_time_cksum();
 }
