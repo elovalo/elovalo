8e610604eee866cda80d2ea248d704d62c96938a
Juho Vepsäläinen <bebraw@gmail.com>
2012-09-01 17:25:25 +0300

    Make it possible to render specific animation length

--- simulator/init.py
+++ simulator/init.py
@@ -8,7 +8,7 @@ def parser():
     p = argparse.ArgumentParser()
     p.add_argument('effect')
     p.add_argument('path')
-    p.add_argument('--hd', help="Render in HD", action='store_true')
+    p.add_argument('--hd', help='Render in HD', action='store_true')
 
     return p
 
@@ -16,9 +16,6 @@ def parser():
 def set_env(args):
     os.environ['effect'] = args.effect
     os.environ['path'] = args.path
-
-    if args.hd:
-        os.environ['hd'] = '1'
 
 
 def write_fps(effect, path):
