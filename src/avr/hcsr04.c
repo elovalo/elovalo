c3b656be0b1a56f8dcd4ef01fc230f552576854b
Jukka Kinnunen <jukka.kinnunen@iki.fi>
2012-09-03 16:52:09 +0300

    Prevent the starting of successive measurement cycle before the previous echo has faded away.

--- src/avr/hcsr04.c
+++ src/avr/hcsr04.c
@@ -5,12 +5,14 @@
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <inttypes.h>
+#include "hcsr04.h"
 
 //values for state
 #define ST_IDLE 0
 #define ST_SENDING_START_PULSE 1
 #define ST_WAITING_RESPONSE_PULSE 2
-#define ST_MEASURING 3
+#define ST_MEASURING_RESPONSE_PULSE 3
+#define ST_WAITING_ECHO_FADING_AWAY 4
 
 volatile uint8_t state;
 
@@ -36,30 +38,40 @@ ISR(TIMER1_COMPA_vect) {
 
 		PORTC &= ~(1<<PC5);    // force trailing edge of the pulse
 
-		//re-init TIMER1 for measuring
-		TIMSK1 &= ~(1<<OCIE1A); // disable int
-
+		//re-init TIMER1 for measuring response pulse length
 		TCCR1B &= ~(1<<CS12 | 1<<CS11 | 1<<CS10);   // stop timer
-		TCCR1B &= ~(1<<WGM12); //set normal mode
-		OCR1A = 0;
+		OCR1A = 6000;           // set "top" for 96 ms (16 MHz sysclock in use)
 		TCNT1 = 0;
 		TCCR1B |= (1<<CS12);   // start timer, set clock divider to 256
 
 		state = ST_WAITING_RESPONSE_PULSE;
 	}
-	//else{
-		//should not happen
-	//}
+	else {
+		//normally the state should be ST_WAITING_ECHO_FADING_AWAY here
+		//but clear state machine also in all other cases
+
+		if (state == ST_MEASURING_RESPONSE_PULSE){
+			//This situation seems to happen in the next measurement cycle
+			//after an object has disappeared from the beam area
+			resp_pulse_length = HCSR04_MEAS_FAIL;
+		}
+
+		TIMSK1 &= ~(1<<OCIE1A); // disable int
+		PCICR &= ~(1<<PCIE1); //Disable PIN Change Interrupt 1
+		state = ST_IDLE;
+	}
 }
 
 ISR(PCINT1_vect){
+
 	if (state == ST_WAITING_RESPONSE_PULSE){
 		resp_pulse_length = 0 - TCNT1; //underflow on purpose
-		state = ST_MEASURING;
+		state = ST_MEASURING_RESPONSE_PULSE;
 	}
-	else if (state == ST_MEASURING){
+	else if (state == ST_MEASURING_RESPONSE_PULSE){
 		resp_pulse_length += TCNT1;
-		state = ST_IDLE;
+		PCICR &= ~(1<<PCIE1); //Disable PIN Change Interrupt 1
+		state = ST_WAITING_ECHO_FADING_AWAY;
 	}
 	//else{
 		//should not happen
@@ -82,16 +94,21 @@ void pin_init(void){
 }
 
 void hcsr04_init(void){
-	timer_init();
-	pin_init();
+	state = ST_IDLE;
 }
 
-void hcsr04_send_pulse(void){
+int hcsr04_send_pulse(void){
+	if (state != ST_IDLE)
+		return 0;
+
+	pin_init();
 	PORTC |= (1<<PC5);     // start initiate pulse
 	timer_init();
+
 	state = ST_SENDING_START_PULSE;
 
 	TCNT1 = 0;
+	return 1;
 }
 
 uint16_t hcsr04_get_pulse_length(void){
--- src/avr/hcsr04.h
+++ src/avr/hcsr04.h
@@ -1,8 +1,16 @@
 #ifndef HCSR04_H_
 #define HCSR04_H_
 
+//return value for hcsr04_get_pulse_length() when measurement failed
+#define HCSR04_MEAS_FAIL 0xFFFF
+
+//initializes the driver state machine
 void hcsr04_init(void);
-void hcsr04_send_pulse(void);
+
+//returns 1 on success, 0 on failure
+int hcsr04_send_pulse(void);
+
+//returns the pulse length, or HCSR04_MEAS_FAIL on failure
 uint16_t hcsr04_get_pulse_length(void);
 
 #endif /* HCSR04_H_ */
