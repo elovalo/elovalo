b3417c4a54774bfcf4527a9c2823ff948a999855
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-09-05 19:55:21 +0300

    Live coding: Implement awesome grayscales

--- src/effects/demo.c
+++ src/effects/demo.c
@@ -4,6 +4,8 @@
 
 void effect(void)
 {
+	// TODO use real kernels instead of loops!
+	
 	clear_buffer();
 
 	for (uint8_t x=0; x<8; x++) {
@@ -13,13 +15,20 @@ void effect(void)
 		}
 	}
 
-	const uint8_t water = (ticks >> 5) & 7;
+	const uint8_t water = ((ticks >> 7) % 7) + 1;
+	const uint8_t surface = ticks & 127;
 
 	for (uint8_t z=0; z<water; z++) {
 		for (uint8_t x=0; x<8; x++) {
 			for (uint8_t y=0; y<8; y++) {
 				set_led(x,y,7-z, MAX_INTENSITY);
 			}
+		}
+	}
+
+	for (uint8_t x=0; x<8; x++) {
+		for (uint8_t y=0; y<8; y++) {
+			set_led(x,y,8-water, surface << 5);
 		}
 	}
 }
