6f9f8161a07bc9343ba311ed9fbfddea105e2eb1
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-09-05 19:41:34 +0300

    Implement rain effect while live-coding

--- src/effects/demo.c
+++ src/effects/demo.c
@@ -5,9 +5,21 @@
 void effect(void)
 {
 	clear_buffer();
-	set_led(0, 0, (ticks >> 2) & 7, MAX_INTENSITY);
-	set_led(0, 7, (ticks >> 2) & 7, MAX_INTENSITY);
-	set_led(7, 0, (ticks >> 2) & 7, MAX_INTENSITY);
-	set_led(7, 7, (ticks >> 2) & 7, MAX_INTENSITY);
-	//scroll_text("Elovalo loves U", 7, MAX_INTENSITY, 10-(ticks >> 3));
+
+	for (uint8_t x=0; x<8; x++) {
+		for (uint8_t y=0; y<8; y++) {
+			srand(5*x+y*3);
+			set_led(x,y,((ticks >> 3)+rand()) & 7, MAX_INTENSITY >> 3);
+		}
+	}
+
+	const uint8_t water = (ticks >> 5) & 7;
+
+	for (uint8_t z=0; z<water; z++) {
+		for (uint8_t x=0; x<8; x++) {
+			for (uint8_t y=0; y<8; y++) {
+				set_led(x,y,7-z, MAX_INTENSITY);
+			}
+		}
+	}
 }
