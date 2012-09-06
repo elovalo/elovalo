6154336ccd2e317115940ab2d8431c05a5eb98d3
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-09-06 18:52:26 +0300

    Calendar support, untested.

--- src/avr/schedule.c
+++ src/avr/schedule.c
@@ -2,15 +2,16 @@
  * Functions for accessing schedule
  */
 
+#include <stdlib.h>
+#include <avr/eeprom.h>
 #include "../pgmspace.h"
 #include "timer.h"
 #include "schedule.h"
 #include "powersave.h"
 #include "serial.h" // For temporary debug code
 
-#define EEPROM_SCHEDULE_POS (event_t*)1
-#define MINUTE_NOT_SET 0xffff
-#define SECS_IN_DAY (60*60*24)
+#define EEPROM_SCHEDULE_POS (struct event *)1
+#define SECS_IN_DAY ((time_t)60*60*24)
 
 void serial_hello(void);
 
@@ -20,51 +21,65 @@ const action_t actions[] PROGMEM = {
 	serial_hello
 };
 
-typedef struct {
-	enum kind {
-		DAILY   = 0x00, // Weekly caledar
-		ONETIME = 0x01, // One-time event
-		EMPTY   = 0xfe, // Not set, go to next
-		END     = 0xff  // End of schedule
-	};
+enum event_kind {
+	DAILY   = 0x00, // Weekly caledar
+	ONETIME = 0x01, // One-time event
+	EMPTY   = 0xfe, // Not set, go to next
+	END     = 0xff  // End of schedule
+};
+
+struct event {
+	enum event_kind kind;
+	uint8_t act; // Action to run
 	union {
 		struct {
 			uint8_t weekdays; // Thursday is LSB
 			int16_t minutes;  // Minutes from midnight, UTC
-			uint8_t act;      // Action to run
 		} daily;
 		struct {
 			time_t ts;   // Timestamp of execution
-			uint8_t act; // Index of function to execute
 		} onetime;
 	} u;
-} event_t;
+};
 
 void periodic_check() {
 	static time_t last_time = 0;
 
-	// Some calculations
+	// Some calculations. TODO if interrupts are enabled, do not use this unsafe thingy
 	time_t start_of_day = unsafe_time(NULL) / SECS_IN_DAY * SECS_IN_DAY;
 	int16_t last_min = (last_time < start_of_day)
 		? -1
 		: (last_time-start_of_day)/60;
 	int16_t now_min = (unsafe_time(NULL) - start_of_day)/60;
-	uint8_t weekday = unsafe_time(NULL) / SECS_IN_DAY % 7;
+	uint8_t weekday = (uint8_t)(unsafe_time(NULL) / SECS_IN_DAY % 7);
 
-	event_t *el = EEPROM_SCHEDULE_POS;
-	while (el++) {
-		event_t event = eeprom_read_event(el);
-		if (event.kind == END) break;
-		if (event.kind == EMPTY) continue;
-		if (event.kind == DAILY) {
+	struct event *eeprom_p = EEPROM_SCHEDULE_POS;
+	while (eeprom_p++) {
+		struct event e;
+		eeprom_read_block(&e,eeprom_p,sizeof(struct event));
+		
+		// Doing the checks
+		if (e.kind == END) break;
+		if (e.kind == EMPTY) continue;
+		if (e.kind == DAILY) {
 			// If not this day, skip
-			if (!(event.weekdays & (1 << weekday))) continue;
+			if (!(e.u.daily.weekdays & (1 << weekday))) continue;
 			// If haven't happened, skip
-			if (event.minutes <= last_min) continue;
-			if (event.minutes > now_min) continue;
-			// Otherwise, run.
-			(action_t)pgm_get(actions[event.act],word)();
+			if (e.u.daily.minutes <= last_min) continue;
+			if (e.u.daily.minutes > now_min) continue;
+		}
+		else if (e.kind == ONETIME) {
+			if (e.u.onetime.ts <= last_time) continue;
+			if (e.u.onetime.ts > unsafe_time(NULL)) continue;
+		}
+		
+		// We got a match, run the action.
+		action_t act = (action_t)pgm_get(actions[e.act],word);
+		act();
 	}
+
+	last_time = unsafe_time(NULL);
+}
 
 void serial_hello(void) {
 	serial_send('h');
