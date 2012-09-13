6d139c881577542658b401b432984faa0ed813ca
Marko Silokunnas <marko.silokunnas@gmail.com>
2012-09-13 19:19:49 +0300

    Add GPLv3 license text to source files

--- src/avr/main.c
+++ src/avr/main.c
@@ -1,11 +1,20 @@
-/**
- * Ledikuutio Main...
- * TODO: 	Make sure that SRAM usage stays as low as possible,
- * 			avoid using local variables or declaring variables at runtime.
- *
- * TODO: 	Check if there's way to monitor the stack usage at runtime
- * 			to avoid stack overflow when pushing close the SRAM limit.
- *
+/*
+ *  Copyright 2012 Elovalo project group 
+ *  
+ *  This file is part of Elovalo.
+ *  
+ *  Elovalo is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Elovalo is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with Elovalo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <avr/interrupt.h>
