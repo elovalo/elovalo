3a3db5c52338e410803996009b4306d0917507c1
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-09-15 23:17:21 +0300

    Adding time.h which is compatible in both AVR and exporter.

---
+++ src/time.h
@@ -0,0 +1,27 @@
+/*
+ *  POSIX compatible real time functions. Remember that time_t may be
+ *  64 bit on POSIX platforms but is uint32_t in AVR.
+ *
+ *  Copyright 2012 Elovalo project group 
+ *  
+ *  This file is part of Elovalo.
+ *  
+ *  Elovalo is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Elovalo is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with Elovalo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef AVR
+#include "avr/clock.h"
+#else
+#include <time.h>
+#elseif
