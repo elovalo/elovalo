da0386ac25e1527b62aa4b65fdabad759bb55f98
Juho Vepsäläinen <bebraw@gmail.com>
2012-09-15 20:28:47 +0300

    Implement circle shape

--- src/effects/circle.c
+++ src/effects/circle.c
@@ -17,11 +17,10 @@
  *  along with Elovalo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-void circle_shape(int8_t xi, int8_t yi, int8_t zi, uint16_t intensity);
-void fish_shape(uint8_t xi, uint8_t yi, uint8_t zi, uint16_t intensity);
-void heart_shape(void);
-void sphere_shape(float xi, float yi, float zi, float rsq_min, float rsq_max, float fac);
-void line(uint8_t x1, uint8_t y1, uint8_t z1, uint8_t x2, uint8_t y2, uint8_t z2,
-	uint16_t intensity);
-void cube_shape(uint8_t x1, uint8_t y1, uint8_t z1, uint8_t x2, uint8_t y2, uint8_t z2,
-	uint16_t intensity);
+# pragma FLIP
+
+#include "common.h"
+
+void effect(void) {
+	circle_shape(0, 0, 0, MAX_INTENSITY);
+}
