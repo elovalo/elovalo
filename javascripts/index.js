5a3ccedd480fef4aa7cd0c14beac7d73298bd631
Juho Vepsäläinen <bebraw@gmail.com>
2012-10-29 10:50:32 +0200

    Add news header

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,6 @@
 require(['jquery', 'rss', 'jquery.caro.min'], function($, rss) {
     $(function() {
-        var amount = 8;
+        var amount = 7;
 
         rss.widget($('.news'), ['https://www.facebook.com/feeds/page.php?format=rss20&id=272782769499260'], amount, amount);
         $('.details').caro({naviClass: 'navi', cycle: true});
