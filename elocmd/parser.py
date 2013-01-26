9f92250c27101af82067d705f8fb3a23ed8c4b82
Joni Katajamäki <joni.katajamaki@gmail.com>
2013-01-26 15:52:48 +0200

    Fix syntax errors and incorrect baudrate in Elocmd

--- elocmd/parser.py
+++ elocmd/parser.py
@@ -145,7 +145,7 @@ class EloParser():
     def parse_flip(self):
         self.parse_response()
         r = self.resp_data
-        return config.Response.FLIP in r
+        return config.Report.FLIP in r
 
     def parse_ok(self):
         self.parse_response()
